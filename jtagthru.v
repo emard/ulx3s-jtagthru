e16d24d5d7a082d3253e4e996e31efaa98a06be4
emard <vordah@gmail.com>
2019-01-28 00:01:38 +0100

    cleanup

--- jtagthru.v
+++ jtagthru.v
@@ -11,23 +11,10 @@ module jtagthru
     assign wifi_gpio0 = btn[0];
 
     wire ftdi_rxd, ftdi_txd, ftdi_nrts, ftdi_ndtr;
-
     wire jtag_tdo, jtag_tdi, jtag_tms, jtag_clk;
-
-    //(* LOC="U17" *) (* IO_TYPE="LVCMOS33" *) // gn14
-    //TRELLIS_IO #(.DIR("INPUT")) jtag_tdo_buf (.B(jtag_tdo_pin), .O(jtag_tdo));
     assign jtag_tdo = gn[14];
-
-    //(* LOC="N17" *) (* IO_TYPE="LVCMOS33" *) // gp15
-    //TRELLIS_IO #(.DIR("OUTPUT")) jtag_tdi_buf (.B(jtag_tdi_pin), .I(jtag_tdi));
     assign gp[15] = jtag_tdi;
-
-    //(* LOC="U18" *) (* IO_TYPE="LVCMOS33" *) // gp14
-    //TRELLIS_IO #(.DIR("OUTPUT")) jtag_tms_buf (.B(jtag_tms_pin), .I(jtag_tms));
     assign gp[14] = jtag_tms;
-
-    //(* LOC="P16" *) (* IO_TYPE="LVCMOS33" *) // gn15
-    //TRELLIS_IO #(.DIR("OUTPUT")) jtag_clk_buf (.B(jtag_clk_pin), .I(jtag_clk));
     assign gn[15] = jtag_clk;
 
     localparam ctr_width = 28;
